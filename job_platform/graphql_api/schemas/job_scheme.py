f231a590994ec81c76ecb4851caba0f1affa9abc
Əbil Abilli <ebilebilli3@gmail.com>
2025-05-31 00:14:54 +0400

    New views(resolver) for Job model

--- job_platform/graphql_api/schemas/job_scheme.py
+++ job_platform/graphql_api/schemas/job_scheme.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from jobs.models.job_model import Job
 
 
@@ -12,6 +12,21 @@ class JobType(DjangoObjectType):
 
 class JobQuery(graphene.ObjectType):
     all_jobs = graphene.List(JobType)
+    job = graphene.Field(JobType, id=graphene.ID(required=True))
+    jobs_by_user = graphene.List(JobType, employer_id=graphene.ID(required=True) )
 
     def resolve_all_jobs(self, info):
         return Job.objects.all()
+    
+    def resolve_job(self, info, id):
+        try:
+            return Job.objects.get(id=id)
+        except ObjectDoesNotExist:
+            raise ValidationError({'error': 'Jobs not found'})
+            
+
+    def resolve_jobs_by_user(self, info, employer_id):
+        try:
+            return Job.objects.filter(employer_id=employer_id)
+        except ObjectDoesNotExist:
+            raise ValidationError({'error': 'Jobs not found for this employer'})
